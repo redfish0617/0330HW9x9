f7de066ceae1a70b6588a6265644cb8ab05f1d2a
redfish0617 <acds10230323@gmail.com>
2020-03-30 16:03:50 +0800

    '9x9'

---
+++ 0330HW9X9/Controllers/HomeworkController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _0330HW9X9.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        // GET: Homework
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 0330HW9X9/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_0330HW9X9.MvcApplication" Language="C#" %>
